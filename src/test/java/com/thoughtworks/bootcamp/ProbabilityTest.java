ba16863c6989b6d0848d977c440cb6f8575c8ee9
avanijayantkale <Avanikale20>
2019-09-05 09:18:46 +0530

    [Avani] Add. Behaviour Of Two Different Probability Occuring Together

--- src/test/java/com/thoughtworks/bootcamp/ProbabilityTest.java
+++ src/test/java/com/thoughtworks/bootcamp/ProbabilityTest.java
@@ -28,32 +28,73 @@ public class ProbabilityTest {
     }
 
     @Test
-    void givenOneProbability_WhenChecksUnoccuredProbability_ThenReturnsZero() {
+    void givenOneProbability_WhenChecksNotoccuredProbability_ThenReturnsZero() {
         assertEquals(impossibleEvent, certainEvent.not());
     }
 
     @Test
-    void givenTwoZeroProbabilites_WhenCalculatingProbabilityOfEventsOccuringTogether_ThenReturnsZero() {
+    void givenTwoZeroProbabilites_WhenCalculatingProbabilityOfEventsOccurringTogether_ThenReturnsZero() {
         assertEquals(impossibleEvent, impossibleEvent.and(impossibleEvent));
     }
 
-    //
+
     @Test
-    void givenOneEventOfProbabilityOneAndAnotherProbabilityZeroDotOne_WhenCalculatingProbabilityOfEventsOccuringTogether_ThenReturnsZeroDotOne() {
+    void givenOneEventOfProbabilityOneAndAnotherProbabilityZeroDotOne_WhenCalculatingProbabilityOfEventsOccurringTogether_ThenReturnsZeroDotOne() {
 
         Probability ZeroDotOneProbability = new Probability(0.1f);
         assertEquals(new Probability(0.1f), ZeroDotOneProbability.and(certainEvent));
     }
 
     @Test
-    void givenTwoOneProbabilityEvents_WhenCalculatingProbabilityOfEventsOccuringTogether_ThenReturnsOne() {
+    void givenTwoOneProbabilityEvents_WhenCalculatingProbabilityOfEventsOccurringTogether_ThenReturnsOne() {
         Probability headInACoinToss = new Probability(0.5f);
         assertEquals(new Probability(0.25f), headInACoinToss.and(headInACoinToss));
     }
 
     @Test
-    void givenOneImposibleEventsAndCertainEvents_WhenCalculatingProbabilityEitherOfTwoEventsOccure_ThenOne() {
+    void givenTwoDifferentProbabilityOfTwoEvent_WhenCalculatedOfEventOccurringTogether_ThenReturnZeroPointTwoFive() {
+        Probability headInACoinToss = new Probability(0.5f);
+        Probability tailInACoinToss = new Probability(0.5f);
+        assertEquals(new Probability(0.25f), headInACoinToss.and(tailInACoinToss));
+    }
+
+    @Test
+    void givenTwoDifferentProbabilityOfTwoEvent_WhenCalculatedOfEventOccurringTogether_ThenReturnZeroPointTwoZero() {
+        Probability headInACoinToss = new Probability(0.4f);
+        Probability tailInACoinToss = new Probability(0.5f);
+        assertEquals(new Probability(0.20f), headInACoinToss.and(tailInACoinToss));
+    }
+
+    /**
+     * Doubt Not return 0.06 and  but its return 0.20f
+     */
+//    @Test
+//    void givenTwoDifferentProbabilityOfTwoEvent_WhenCalculatedOfEventOccurringTogether_ThenReturn() {
+//        Probability headInACoinToss = new Probability(0.2f);
+//        Probability tailInACoinToss = new Probability(0.3f);
+//        assertEquals(new Probability(0.06f), headInACoinToss.and(tailInACoinToss));
+//    }
+    @Test
+    void givenOneImpossibleEventsAndCertainEvents_WhenCalculatingProbabilityEitherOfTwoEventsOccur_ThenReturnOne() {
         assertEquals(certainEvent, impossibleEvent.or(certainEvent));
+    }
+
+    @Test
+    void givenTwoImpossibleEvents_WhenCalculatingProbabilityEitherOfTwoEventsOccur_ThenReturnZero() {
+        assertEquals(impossibleEvent, impossibleEvent.or(impossibleEvent));
+    }
+
+    @Test
+    void givenTwoCertainEvents_WhenCalculatingProbabilityEitherOfTwoEventsOccur_ThenReturnOne() {
+        assertEquals(certainEvent, certainEvent.or(certainEvent));
+    }
+
+    @Test
+    void givenDifferentProbabilitiesOfTwoEvents_WhenCalculatingProbabilityOfEitherOfTwoEventsOccur_ThenReturnZeroPointFour() {
+        Probability evenNoOfRollingDice = new Probability(0.2f);
+        Probability oddNoOfRollingDice = new Probability(0.3f);
+        assertEquals(new Probability(0.44f), evenNoOfRollingDice.or(oddNoOfRollingDice));
+
     }
 
 
