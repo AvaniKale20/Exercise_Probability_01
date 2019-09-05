6243e00d1d6593ea9b7e8efbeca61329235424c5
avanijayantkale <Avanikale20>
2019-09-05 06:53:56 +0530

    [Avani] Add. Behaviour of two differnts Event Calculate Probability Either of two events occure

--- src/main/java/com/thoughtworks/bootcamp/Probability.java
+++ src/main/java/com/thoughtworks/bootcamp/Probability.java
@@ -1,18 +1,30 @@
 package com.thoughtworks.bootcamp;
 
 public class Probability {
-    private float EventOne;
+    private final float value;
 
-
-    public Probability(float EventOne) {
-        this.EventOne = EventOne;
+    public Probability(float value) {
+        this.value = value;
     }
 
+    @Override
     public boolean equals(Object object) {
-        return true;
+        if (!(object instanceof Probability)) {
+            return false;
+        }
+        Probability that = (Probability) object;
+        return this.value == that.value;
     }
 
-    public float EqualityOfEvents() {
-        return 0.0f;
+    public Probability not() {
+        return new Probability(1.0f - value);
+    }
+
+    public Probability and(Probability probabilityEvent) {
+        return new Probability(value * probabilityEvent.value);
+    }
+
+    public Probability or(Probability event) {
+        return new Probability(value).not().and(new Probability(event.value).not()).not();
     }
 }
--- src/test/java/com/thoughtworks/bootcamp/ProbabilityTest.java
+++ src/test/java/com/thoughtworks/bootcamp/ProbabilityTest.java
@@ -1,17 +1,61 @@
 package com.thoughtworks.bootcamp;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class ProbabilityTest {
+    private static Probability impossibleEvent;
+    private static Probability certainEvent;
+
+    @BeforeAll
+    static void setUp() {
+        impossibleEvent = new Probability(0.0f);
+        certainEvent = new Probability(1.0f);
+    }
 
     @Test
-    void givenZeroProbabilityOfTwoEvents_WhenCheckEquality_ThenReturnZero() {
-        float zeroProbability = 0.0f;
-        Probability zeroProbabilityEvents1 = new Probability(zeroProbability);
-        Probability zeroProbabilityEvents2 = new Probability(zeroProbability);
-        assertEquals(true, zeroProbabilityEvents1.equals(zeroProbabilityEvents2));
-
+    void givenZeroProbabilitiesOfTwoEvents_WhenChecksEquality_ThenReturnsTrue() {
+        Probability zeroProbabilityEventTwo = new Probability(0.0f);
+        assertEquals(impossibleEvent, zeroProbabilityEventTwo);
     }
+
+    @Test
+    void givenZeroOcuurenceProbability_WhenChecksUnoccuredProbability_ThenReturnsOne() {
+        assertEquals(certainEvent, impossibleEvent.not());
+    }
+
+    @Test
+    void givenOneProbability_WhenChecksUnoccuredProbability_ThenReturnsZero() {
+        assertEquals(impossibleEvent, certainEvent.not());
+    }
+
+    @Test
+    void givenTwoZeroProbabilites_WhenCalculatingProbabilityOfEventsOccuringTogether_ThenReturnsZero() {
+        assertEquals(impossibleEvent, impossibleEvent.and(impossibleEvent));
+    }
+
+    //
+    @Test
+    void givenOneEventOfProbabilityOneAndAnotherProbabilityZeroDotOne_WhenCalculatingProbabilityOfEventsOccuringTogether_ThenReturnsZeroDotOne() {
+
+        Probability ZeroDotOneProbability = new Probability(0.1f);
+        assertEquals(new Probability(0.1f), ZeroDotOneProbability.and(certainEvent));
+    }
+
+    @Test
+    void givenTwoOneProbabilityEvents_WhenCalculatingProbabilityOfEventsOccuringTogether_ThenReturnsOne() {
+        Probability headInACoinToss = new Probability(0.5f);
+        assertEquals(new Probability(0.25f), headInACoinToss.and(headInACoinToss));
+    }
+
+    @Test
+    void givenOneImposibleEventsAndCertainEvents_WhenCalculatingProbabilityEitherOfTwoEventsOccure_ThenOne() {
+        assertEquals(certainEvent, impossibleEvent.or(certainEvent));
+    }
+
+
 }
+
